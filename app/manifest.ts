f80b0f21345fdc6769fafb4bdca279679af3eab2
enricopetracci-spec <enrico.petracci@gmail.com>
2026-03-08 23:32:35 +0100

    Update manifest.ts

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,16 +11,18 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#3B82F6',
     orientation: 'portrait-primary',
     icons: [
-  {
-    src: '/icon-192.png',
-    sizes: '192x192',
-    type: 'image/png',
-    purpose: 'any'
-  },
-  {
-    src: '/icon-512.png',
-    sizes: '512x512',
-    type: 'image/png',
-    purpose: 'any maskable'
+      {
+        src: '/icon-192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any'
+      },
+      {
+        src: '/icon-512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable'
+      }
+    ]
   }
-]
+}
